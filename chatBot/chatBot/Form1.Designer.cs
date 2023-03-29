5dbc5c732abf8b70b7b8749723c15969a203ed30
GaryGarrington <karelberanek2003@gmail.com>
2023-03-29 19:29:47 +0200

    Upload

---
+++ chatBot/chatBot/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace chatBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_send = new System.Windows.Forms.Button();
+            this.txb_in = new System.Windows.Forms.TextBox();
+            this.txb_out = new System.Windows.Forms.TextBox();
+            this.btn_clr = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(538, 416);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 0;
+            this.btn_send.Text = "Odeslat";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // txb_in
+            // 
+            this.txb_in.Location = new System.Drawing.Point(12, 418);
+            this.txb_in.Name = "txb_in";
+            this.txb_in.Size = new System.Drawing.Size(520, 20);
+            this.txb_in.TabIndex = 1;
+            this.txb_in.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txb_in_KeyPress);
+            // 
+            // txb_out
+            // 
+            this.txb_out.Location = new System.Drawing.Point(12, 12);
+            this.txb_out.Multiline = true;
+            this.txb_out.Name = "txb_out";
+            this.txb_out.ReadOnly = true;
+            this.txb_out.Size = new System.Drawing.Size(520, 400);
+            this.txb_out.TabIndex = 2;
+            // 
+            // btn_clr
+            // 
+            this.btn_clr.Location = new System.Drawing.Point(538, 12);
+            this.btn_clr.Name = "btn_clr";
+            this.btn_clr.Size = new System.Drawing.Size(75, 30);
+            this.btn_clr.TabIndex = 3;
+            this.btn_clr.Text = "Smazat";
+            this.btn_clr.UseVisualStyleBackColor = true;
+            this.btn_clr.Click += new System.EventHandler(this.btn_clr_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 450);
+            this.Controls.Add(this.btn_clr);
+            this.Controls.Add(this.txb_out);
+            this.Controls.Add(this.txb_in);
+            this.Controls.Add(this.btn_send);
+            this.Name = "Form1";
+            this.Text = "Chad the ChatBot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox txb_in;
+        private System.Windows.Forms.TextBox txb_out;
+        private System.Windows.Forms.Button btn_clr;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ chatBot/chatBot/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Net;
+
+// MICHAL JE BLB JUST SAYIN'
+
+namespace chatBot
+{
+    public partial class Form1 : Form
+    {
+        bool followup;
+        public void question()
+        {
+            string text;
+            text = txb_in.Text;
+            text = text.ToLower();
+            string date = Convert.ToString(DateTime.Now);
+            string[] pole = date.Split(' ');
+            txb_out.Text += "Ty: " + txb_in.Text + Environment.NewLine;
+            /*if (followup == true)
+            {
+                if (text == "ano")
+                    this.Close();
+                else if (text == "ne")
+                {
+                    txb_out.Text += "Chad: Tak si nestěžuj" + Environment.NewLine;
+                    followup = false;
+                }
+                else
+                {
+                    txb_out.Text += "Chad: Na něco jsem se ptal, ale dobře" + Environment.NewLine;
+                    followup = false;
+                }
+            }*/
+            for (int j = 0; j <= questions.Count; j++)
+                {
+                /*if (text == "už mě nebavíš")
+                    {
+                    txb_out.Text += "Chad: Chceš ukončit program?" + Environment.NewLine;
+                    followup = true;
+                    break;
+                    }*/
+                if (text == "datum")
+                        {
+                            txb_out.Text += "Chad: Dnes je " + pole[0] + Environment.NewLine;
+                            break;
+                        }
+                    if (text == "čas")
+                        {
+                            txb_out.Text += "Chad: Teď je " + pole[1] + Environment.NewLine;
+                            break;
+                        }
+                    if (j == questions.Count)
+                        {
+                            string input = Interaction.InputBox("Tuto otázku neznám: " + txb_in.Text, "Neznámá otázka", "odpověď na tuto otázku", 10, 10);
+                            if (input == "" || input == "odpověď na tuto otázku")
+                                break;
+                            StreamWriter fwrite = new StreamWriter("src.csv", true);
+                            fwrite.WriteLine(text + ";" + input);
+                            fwrite.Close();
+                            questions.Add(text);
+                            answers.Add(input);
+                            break;
+                        }
+                    if (text == questions[j])
+                        {
+                            txb_out.Text += "Chad: " + answers[j] + Environment.NewLine;
+                            break;
+                        }
+                }
+                txb_in.Clear();
+                txb_in.Focus();
+        }
+        List<string> questions = new List<string>();
+        List<string> answers = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            question();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txb_in.Focus(); // nefunguje ffs
+            string line;
+            StreamReader fload = new StreamReader("src.csv");
+            while ((line = fload.ReadLine()) != null)
+            {
+                string[] pole = line.Split(';');
+                questions.Add(pole[0]);
+                answers.Add(pole[1]);
+            }
+            fload.Close();
+        }
+
+        private void btn_clr_Click(object sender, EventArgs e)
+        {
+            txb_out.Clear();
+        }
+
+        private void txb_in_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char guess = e.KeyChar;
+            char.ToUpper(guess);
+            if (guess == (char)Keys.Enter)
+            {
+                question();
+            }
+        }
+    }
+}
